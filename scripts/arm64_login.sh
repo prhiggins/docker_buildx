17c5a9b2ec60c6c5d17f739ba65e26a1819c55e7
Patrick Higgins <phiggins@cs.uoregon.edu>
2020-04-18 23:28:41 -0700

    fix url build

--- scripts/arm64_login.sh
+++ scripts/arm64_login.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
 ARM64_DEVICE_ADDR = $1
-docker context create python-arm64 --docker host=ssh://ARM64_DEVICE_ADDR
+echo $1
+docker context create python-arm64 --docker host=ssh://$ARM64_DEVICE_ADDR
+echo "Created context"
