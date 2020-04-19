65072d983af3f11b8639c5fc4c5918b495c6a9df
Patrick Higgins <phiggins@cs.uoregon.edu>
2020-04-18 18:42:20 -0700

    remove port

--- scripts/arm64_login.sh
+++ scripts/arm64_login.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
-docker context create python-arm64 --docker "host=ssh://$1:2376"
+docker context create python-arm64 --docker "host=ssh://$1"
 docker buildx create --name actions_builder --append python-arm64
