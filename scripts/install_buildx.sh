18c31f5a5534f5904c30169a5ab1d37da0edf68a
Patrick Higgins <phiggins@cs.uoregon.edu>
2020-04-18 23:03:01 -0700

    disable qemu support

--- scripts/install_buildx.sh
+++ scripts/install_buildx.sh
@@ -14,9 +14,9 @@ mv buildx* ~/.docker/cli-plugins/docker-buildx
 chmod a+x ~/.docker/cli-plugins/docker-buildx
 
 # Register Arm executables
-docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3
+# docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3
 # Register Multiarch executables
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # Create builder instance
 docker buildx create --name actions_builder --use
