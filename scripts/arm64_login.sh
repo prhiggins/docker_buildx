37b35d8425722d0d2a30cc6d1882deb2bc504446
Patrick Higgins <phiggins@cs.uoregon.edu>
2020-04-18 23:36:50 -0700

    fix it again

--- scripts/arm64_login.sh
+++ scripts/arm64_login.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-ARM64_DEVICE_ADDR = $1
-echo $1
-docker context create python-arm64 --docker host=ssh://$ARM64_DEVICE_ADDR
+ARM64_HOSTNAME=$1
+echo ARM64_HOSTNAME
+docker context create python-arm64 --docker host=ssh://$ARM64_HOSTNAME
 echo "Created context"
